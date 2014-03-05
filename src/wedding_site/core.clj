4c0c4ecfb87d392c51b7f55afe0b4183fa551b93
Brandon White <brandon.ray.white@gmail.com>
2014-03-04 22:23:02 -0700

    Under construction landing page'

--- src/wedding_site/core.clj
+++ src/wedding_site/core.clj
@@ -1,17 +1,18 @@
 (ns wedding-site.core
   (:gen-class :main :true)
-  (:use [compojure.route :only [files not-found]]
+  (:use [compojure.route :only [files not-found resources]]
       [compojure.handler :only [site]]
       [compojure.core :only [defroutes GET POST]]
       [clostache.parser]
       [org.httpkit.server :only [run-server]]))
 
 (defn show-landing-page [req]
-  (render-resource "templates/index.mustache" {:person "Stinky"})
+  (render-resource "templates/index.mustache" {})
   )
 
 (defroutes all-routes
   (GET "/" [] show-landing-page)
+  (resources "/")
   (not-found "<p>Page not found.</p>")) ;; all other, return 404
 
 (defn -main [port]
